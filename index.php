7c3903bb09c9197f063426bb3d8776187e69f0e6
le sin <sin.letrung@gmail.com>
2022-05-06 10:15:11 +0700

    s

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ require __DIR__ . '/vendor/autoload.php';
 // Or Private App
 $credential = new Slince\Shopify\PrivateAppCredential('6a6af91b3f4ad4b566e07198fefa500f', '7acede28590c06cf00fb666ab876fdb8', '617f6659065b53e31eacb54a6686fd5e');
 
-$client = new Slince\Shopify\Client('testmagicexhalation.myshopify.com', $credential, [
-    'meta_cache_dir' => './tmp' // Metadata cache dir, required
-]);
+// $client = new Slince\Shopify\Client('testmagicexhalation.myshopify.com', $credential, [
+//     'meta_cache_dir' => './tmp' // Metadata cache dir, required
+// ]);
 $product = $client->getProductManager()->findALL();
 print_r($product);
 echo $client->getProductManager()->count();
