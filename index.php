6617cfc24375f4407f12743437e314c68a117320
le sin <sin.letrung@gmail.com>
2022-05-06 14:03:07 +0700

    d

--- index.php
+++ index.php
@@ -3,15 +3,17 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 require __DIR__ . '/vendor/autoload.php';
-
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 //$credential = new Slince\Shopify\PublicAppCredential('Access Token');
 // Or Private App
-$credential = new Slince\Shopify\PrivateAppCredential('6a6af91b3f4ad4b566e07198fefa500f', '7acede28590c06cf00fb666ab876fdb8', '617f6659065b53e31eacb54a6686fd5e');
+$credential = new Slince\Shopify\PrivateAppCredential($_ENV['APIKEYSHOP'], $_ENV['PASSAPISHOP'], '617f6659065b53e31eacb54a6686fd5e');
 
-$client = new Slince\Shopify\Client('testmagicexhalation.myshopify.com', $credential, [
+$client = new Slince\Shopify\Client($_ENV['NAMESHOP'], $credential, [
     'meta_cache_dir' => './tmp/log' // Metadata cache dir, required
 ]);
-$product = $client->getProductManager()->findALL();
-print_r($product);
+//$product = $client->getProductManager()->findALL();
+//print_r($product);
 echo $client->getProductManager()->count();
 ?>
+
