21d23d71c6469017caaba454748a9211c84a6401
le sin <sin.letrung@gmail.com>
2022-05-08 20:10:20 +0700

    s

--- index.php
+++ index.php
@@ -115,7 +115,17 @@ if($func == 'saved'){
 
         }//end while
     }//end if
- ?>  
+ ?> <tr>
+     <td colspan="10"><nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav></td>
+    </tr> 
     </tbody>
   </table>
 <?php
