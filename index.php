62a65a2dfa1462639301bde3f939e79d08a5d3aa
le sin <sin.letrung@gmail.com>
2022-05-09 17:43:05 +0700

    d

--- index.php
+++ index.php
@@ -217,6 +217,8 @@ if($func == 'saved'){
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 <script>
+    //var filename = path.basename('/Users/Refsnes/demo_path.js');
+
     $(document).ready(function() {
 
         $("#btnOptimze").click(function(){
@@ -246,6 +248,9 @@ if($func == 'saved'){
                     .done(function( data ) {
                         console.log("sss");
                         console.log(data);
+                        const obj = JSON.parse(data);
+                        var filename = obj.path_out_new.substr(obj.path_out_new.lastIndexOf("/")+1);
+                        $("#"+imgOpts[0]).html('<div id="loading"><img src="./node/optimalfile/'+ filename +'" alt="Loading..."/></div>');
                     });
             });
             // $('input[name="optimze-check-input"]:checked').each(function() {
