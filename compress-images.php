66effa12f56ee200bbfaec7facd2e6397b714eb5
le sin <sin.letrung@gmail.com>
2022-05-09 15:01:44 +0700

    s

---
+++ compress-images.php
@@ -0,0 +1,40 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+ini_set('max_execution_time', '1800'); //300 seconds = 5 minutes
+
+error_reporting(E_ALL);
+require __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$servername = $_ENV["MYSQLSERVER"];
+$username = $_ENV["MYSQLUSER"];
+$password = $_ENV["MYSQLPASS"];
+$db = $_ENV["MYSQLDB"];
+$localApi = $_ENV["LOCALAPI"];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$image = $_GET['image'];
+$curl = curl_init();
+$url = "http://".$localApi."/optimze/".$data;
+
+// Optional Authentication:
+//    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+//    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec($curl);
+
+curl_close($curl);
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($data);
+?>
