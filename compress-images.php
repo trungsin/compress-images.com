54fa683d466028f4af939191d0229260637a8108
le sin <sin.letrung@gmail.com>
2022-05-09 15:38:34 +0700

    s

--- compress-images.php
+++ compress-images.php
@@ -32,9 +32,9 @@ $url = "http://".$localApi."/optimze/".$image;
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-$result = curl_exec($curl);
+$result = json_decode(curl_exec($curl));
 print_r($result);
-$sql = "UPDATE `product_images` SET `optimalfile` = '".basename($result->path_out_new)."', `originalsize` = ".$result->size_in.", `optimalsize`=".$result->size_output.", `percent`='".$result->percent."%', `timeoptimal`=1 WHERE imageID='".$imageID."'";
+$sql = "UPDATE `product_images` SET `optimalfile` = '".basename($result['path_out_new'])."', `originalsize` = ".$result['size_in'].", `optimalsize`=".$result['size_output'].", `percent`='".$result['percent']."%', `timeoptimal`=1 WHERE imageID='".$imageID."'";
 $conn->query($sql); 
 
 curl_close($curl);
