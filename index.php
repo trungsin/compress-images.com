3c8e960cfb9c38f868a943daa2619867a93149c5
le sin <sin.letrung@gmail.com>
2022-05-10 16:15:04 +0700

    s

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ $products = $client->getProductManager()->findALL();
 //print_r($product);
 $pagination = $client->getProductManager()->paginate([
     // // filter your product
-     'limit' => 1000,
+     'limit' => 250,
     // 'created_at_min' => '2015-04-25T16:15:47-04:00'
 ]);
 // $pagination is instance of `Slince\Shopify\Common\CursorBasedPagination`
