b65db7ae4808d973ce4418f4924cd33393a36ead
le sin <sin.letrung@gmail.com>
2022-05-11 08:26:26 +0700

    s

--- applyimage.php
+++ applyimage.php
@@ -38,6 +38,6 @@ $conn = new PDO("mysql:host=".$servername.";dbname=".$db, $username, $password);
  $row = $sth->fetch();
  //print_r($row)
  echo 'http://compress-images.com/node/optimalfile/'.$row['optimalfile'];
-//$newimage = $client->getProductImageManager()->create($productID,array ('src' => 'http://compress-images.com/node/optimalfile/'.$row['optimalfile']));
-//print_r($image);
+$newimage = $client->getProductImageManager()->create($productID,array ('src' => 'http://compress-images.com/node/originalfiles/'.$row['optimalfile']));
+print_r($image);
 ?>
