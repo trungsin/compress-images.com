e99dc9fb4d30035982700cf6a5319ad2bc64d8f9
le sin <sin.letrung@gmail.com>
2022-06-18 15:59:23 +0700

    s

--- inc/dashboard.php
+++ inc/dashboard.php
@@ -16,7 +16,7 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Total Image  Crawl From Memory</div>
+                                                <a href="/?func=saved">Total Image  Crawl From Memory</a></div>
                                             <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $totalImage;?></div>
                                         </div>
                                         <div class="col-auto">
