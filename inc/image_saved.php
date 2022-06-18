032fb229701c487101b95ad821372ee8bc16f6f7
le sin <sin.letrung@gmail.com>
2022-06-18 16:04:02 +0700

    s

--- inc/image_saved.php
+++ inc/image_saved.php
@@ -13,19 +13,15 @@ if (isset($_GET['page']))
     //$result = $conn->query($sql);
     ?>
 <!-- Page Heading -->
-<h1 class="h3 mb-2 text-gray-800">Tables</h1>
-<p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-    For more information about DataTables, please visit the <a target="_blank"
-        href="https://datatables.net">official DataTables documentation</a>.</p>
-
+<h1 class="h3 mb-2 text-gray-800">Memory</h1>
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+        <h6 class="m-0 font-weight-bold text-primary">Detail Image Crarl from Memory</h6>
     </div>
     <div class="card-body">
         <div class="table-responsive">
-        <table class="table table-dark" style="border-color: aqua;">
+        <table class="table table-bordered" style="border-color: aqua;">
             <thead>
               <tr>
                 <th scope="col" style="width: 300px;">Title Product</th>
