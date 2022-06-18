621850e80befe3f0e6fa60d060c912866a740fd7
le sin <sin.letrung@gmail.com>
2022-06-18 16:01:15 +0700

    d

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ error_reporting(E_ALL);
 require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
+$rootShop = "https://".$_ENV["NAMESHOP"];
 $i=1;
 $servername = $_ENV["MYSQLSERVER"];
 $username = $_ENV["MYSQLUSER"];
@@ -70,7 +70,7 @@ if($func == 'saved'){
     //$credential = new Slince\Shopify\PublicAppCredential('Access Token');
     // Or Private App
     $credential = new Slince\Shopify\PrivateAppCredential($_ENV['APIKEYSHOP'], $_ENV['PASSAPISHOP'], '617f6659065b53e31eacb54a6686fd5e');
-    $rootShop = "https://".$_ENV["NAMESHOP"];
+    
     $client = new Slince\Shopify\Client($_ENV['NAMESHOP'], $credential, [
         'meta_cache_dir' => './tmp/log' // Metadata cache dir, required
     ]);
