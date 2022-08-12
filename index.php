4fbb91a84da7bd40849cf9a07412926564cf591f
le sin <sin.letrung@gmail.com>
2022-08-12 20:02:47 +0700

    update

--- index.php
+++ index.php
@@ -136,7 +136,7 @@ if($func == 'saved'){
     if($chanel != ""){
         $query = array("status" => "any","note"=>$chanel);    
     } else {
-        $query = array("status" => "any",'created_at_max'=>date('Y-M-D',now()));//,'created_at_min'=>'2022-07-30','created_at_max'=>'2022-07-30');
+        $query = array("status" => "any",'created_at_max'=>date('Y-M-D'));//,'created_at_min'=>'2022-07-30','created_at_max'=>'2022-07-30');
     }
     
     $orders = $client->getOrderManager()->findAll($query);//.$orderID.".1"]);
