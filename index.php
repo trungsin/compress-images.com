9612cd164a3a89197fb366cc17b2d0a0c3c8192d
le sin <sin.letrung@gmail.com>
2022-08-12 16:49:49 +0700

    s

--- index.php
+++ index.php
@@ -136,7 +136,7 @@ if($func == 'saved'){
     if($chanel != ""){
         $query = array("status" => "any","note"=>$chanel);    
     } else {
-        $query = array("status" => "any",'created_at_min'=>'2022-07-29','created_at_max'=>'2022-07-29');
+        $query = array("status" => "any",'created_at_min'=>'2022-07-20','created_at_max'=>'2022-07-29');
     }
     
     $orders = $client->getOrderManager()->findAll($query);//.$orderID.".1"]);
